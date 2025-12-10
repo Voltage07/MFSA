f5fa4cfbe418d4ec99414a0ddcdb97fff443e3d2
Desmond Osahon <desmondigbinokhuo@gmail.com>
2025-12-10 21:36:21 +0100

    first header swiper

--- assets/scripts/m.js
+++ assets/scripts/m.js
@@ -1,14 +1,21 @@
 // Runs early — after HTML is ready
 document.addEventListener("DOMContentLoaded", () => {
 
+    // Intersection Observer options
+const options = {
+  root: null,
+  rootMargin: "0px 0px -100px 0px",
+  threshold: 0.3           
+};
     // scroll reveal
     const observer = new IntersectionObserver(entries => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
                 entry.target.classList.add("show");
+                observer.unobserve(entry.target);   // only once
             }
         });
-    });
+    },options);
 
     document.querySelectorAll('.reveal').forEach(el => observer.observe(el));
 
