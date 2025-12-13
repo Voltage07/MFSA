21bafb78db9421888959f4cfa7fb58bf47e2a810
Desmond Osahon <desmondigbinokhuo@gmail.com>
2025-12-13 12:12:11 +0100

    faq

--- assets/scripts/m.js
+++ assets/scripts/m.js
@@ -6,7 +6,7 @@ const options = {
   root: null,
 //   rootMargin: "0px 0px -100px 0px",
 //   threshold: 0.3   
-threshold: 0.2,
+threshold: 0.15,
 rootMargin: "0px 0px -40px 0px"        
 };
     // scroll reveal
